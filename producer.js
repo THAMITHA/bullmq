535a2fba92c8b7645620a95516763bdaf5780ed8
THAMITHA S <THAMITHA.S@ibm.com>
2023-12-04 12:52:20 +0530

    add redis port

--- producer.js
+++ producer.js
@@ -1,6 +1,11 @@
 const {Queue} = require('bullmq');
 
-const notificationQueue =  new Queue('email-queue');
+const notificationQueue =  new Queue('email-queue',{
+    connection:{
+        host:'127.0.0.1',
+        port:'6379'
+    }
+});
 
 async function init(){
     const res = notificationQueue.add('email to thamitha',{
